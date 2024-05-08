a5230c49fc2ac7532d4740f48d4079fa68958101
chri2tine <christineyuan5@gmail.com>
2024-05-08 14:50:10 -0500

    website addition

--- docs/send_email.php
+++ docs/send_email.php
@@ -4,10 +4,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
+    if(empty($name) || empty($email) || empty($message)) {
+        echo "Please fill out all fields.";
+        exit;
+    }
+
     $to = "christineyuan@utexas.edu";
     $subject = "Contact Form Submission Personal Website";
     $headers = "From: $name <$email>";
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
 
-    mail($to, $subject, $message, $headers);
+    if(mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email failed to send.";
+    }
 }
 ?>
